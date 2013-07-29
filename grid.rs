86f5ff19a145f892b7f6d74be79354fb6176bcea
jbaldwin <joshrbaldwin@gmail.com>
2013-07-28 22:15:57 -0700

    moved grid to struct + impl

---
+++ grid.rs
@@ -0,0 +1,156 @@
+#[link(name = "grid", vers = "1.0", author = "jbaldwin")];
+#[crate_type = "lib"];
+
+extern mod std;
+extern mod extra;
+
+use std::path;
+use std::io;
+use std::int;
+
+pub enum CellState {
+	Alive,
+	Dead
+}
+
+pub struct Cell {
+	pub state: CellState
+}
+
+impl Cell {
+	pub fn alive(&self, neighbours_alive: int) -> CellState {
+
+		match self.state {
+			Alive => match neighbours_alive {
+				2 | 3 => Alive,	// Rule #2
+				_ => Dead		// Rules #1, #3
+			},
+			Dead => match neighbours_alive {
+				3 => Alive,		// Rule #4
+				_ => Dead
+			}
+		}
+	}
+}
+
+pub struct Grid {
+	curr: ~[~[Cell]],
+	next: ~[~[Cell]]
+}
+
+impl Grid {
+
+	pub fn width(&self) -> int { self.curr.len() as int }
+	pub fn height(&self) -> int { self.curr[0].len() as int }
+
+	pub fn step(&mut self) {
+		/*
+		* Rules
+		* 1) Any live cell with fewer than two live neighbours dies.
+		* 2) Any live cell with two or three live neighbours lives.
+		* 3) Any live cell with more than three live neighbours dies.
+		* 4) Any dead cell with exactly three live neighbours becomes a live cell.
+		*/
+
+		let neighbours = [
+			[-1, -1],
+			[ 0, -1],
+			[ 1, -1],
+			[-1,  0],
+			[ 1,  0],
+			[-1,  1],
+			[ 0,  1],
+			[ 1,  1]
+		];
+
+		for int::range(0, self.width()) |row| {
+			for int::range(0, self.height()) |col| {
+				let mut count = 0;
+
+				for int::range(0, neighbours.len() as int) |n| {
+					let x = neighbours[n][0] + row;
+					let y = neighbours[n][1] + col;
+
+					if self.in_bounds(x, y) {
+						count +=  match self.curr[x][y].state {
+							Dead => 0,
+							Alive => 1,
+							
+						};
+					}
+
+					// stop counting, the cell is dead
+					if count >= 4 { break; }
+				}
+				self.next[row][col].state = self.curr[row][col].alive(count);
+			}
+		}
+
+		self.copy_back();
+	}
+
+	/**
+	* Loads the input grid file.
+	* File format is line delmited '0's and '1's.
+	* 1 = The square is true|on in the game of life.
+	* 0 = The square is false|off in the game of life.
+	**/
+	pub fn load(file: &path::Path) -> ~Grid {
+
+		let read_result = io::file_reader(file);
+
+		let lines = match read_result {
+			Ok(file) => file.read_lines(),
+			Err(e) => {
+				fail!(fmt!("Error reading input grid file: %?", e))
+			}
+		};
+
+		let mut g: ~Grid = ~Grid { curr: ~[], next: ~[] };
+
+		for lines.iter().advance() |line: &~str| {
+			let mut row1: ~[Cell] = ~[];
+			let mut row2: ~[Cell] = ~[];
+			for line.iter().advance() |c| {
+				let cell: Cell = match c {
+					'0' => Cell { state: Dead },
+					'1' => Cell { state: Alive },
+					_ => fail!(fmt!("Error parsing input grid file: %c is not valid, 0 or 1 only.", c))
+				};
+				row1.push(cell);
+				row2.push(cell);
+			}
+			g.curr.push(row1);
+			g.next.push(row2);
+		}
+
+		return g;
+	}
+
+
+	pub fn print(&self) {
+		for self.curr.iter().advance() |row| {
+			for row.iter().advance() |cell| {
+				print(fmt!("%c", match cell.state {
+					Dead => '-',
+					Alive => '*'
+				}));
+			}
+			println("");
+		}
+	}
+
+	fn in_bounds(&self, x: int, y: int) -> bool {
+		if x >= 0 && x < self.width() &&
+		   y >= 0 && y < self.height() 
+		   { true } else { false }
+	}
+
+	fn copy_back(&mut self) {
+		for int::range(0, self.width()) |i| {
+			for int::range(0, self.height()) |j| {
+				self.curr[i][j].state = self.next[i][j].state;
+			}
+		}
+	}
+}
--- main.rs
+++ main.rs
@@ -2,135 +2,16 @@ extern mod extra;
 
 use std::os;
 use std::path;
-use std::io;
-use std::int;
 use extra::getopts::*;
 
 mod util;
-
+mod grid;
 
 fn print_usage(program_name: &str, _opts: &[Opt]) {
 	println(fmt!("Usage: %s [options]", program_name));
 	println("  -i\t\t-> Path to input file");
 	println("  -h | -help\t-> Print this help message");
 }
-
-/**
-* Loads the input grid file.
-* File format is line delmited '0's and '1's.
-* 1 = The square is true|on in the game of life.
-* 0 = The square is false|off in the game of life.
-**/
-fn load(file: &path::Path) -> ~[~[bool]] {
-
-	let read_result = io::file_reader(file);
-
-	let lines = match read_result {
-		Ok(file) => file.read_lines(),
-		Err(e) => {
-			fail!(fmt!("Error reading input grid file: %?", e))
-		}
-	};
-
-	let mut grid: ~[~[bool]] = ~[];
-
-	for lines.iter().advance() |line: &~str| {
-		let mut next: ~[bool] = ~[];
-		for line.iter().advance() |c| {
-			let b: bool = match c {
-				'0' => false,
-				'1' => true,
-				_ => fail!(fmt!("Error parsing input grid file: %c is not valid, 0 or 1 only.", c))
-			};
-			next.push(b)
-		}
-		grid.push(next);
-	}
-
-	return grid;
-}
-
-fn print_grid(grid: &[~[bool]]) {
-	for grid.iter().advance() |row| {
-		for row.iter().advance() |b| {
-			print(fmt!("%c", match b {
-				&false => '-',
-				&true => '*'
-			}));
-		}
-		println("");
-	}
-}
-
-fn copy_grid(src: &[~[bool]], dst: &mut[~[bool]]) {
-	for int::range(0, src.len() as int) |i| {
-		for int::range(0, src[i].len() as int) |j| {
-			dst[i][j] = src[i][j];
-		}
-	}
-}
-
-fn alive(currently_alive: bool, neighbours_alive: int) -> bool {
-
-	match currently_alive {
-		true => match neighbours_alive {
-			2 | 3 => true,	// Rule #2
-			_ => false		// Rules #1, #3
-		},
-		false => match neighbours_alive {
-			3 => true,		// Rule #4
-			_ => false
-		}
-	}
-}
-
-fn in_bounds(x: int, y: int, width: int, height: int) -> bool {
-	if x >= 0 && x < width && y >= 0 && y < height { true } else { false }
-}
-
-fn game_of_life_step(grid: &mut[~[bool]],  next: &mut[~[bool]]) {
-	/*
-	* Rules
-	* 1) Any live cell with fewer than two live neighbours dies.
-	* 2) Any live cell with two or three live neighbours lives.
-	* 3) Any live cell with more than three live neighbours dies.
-	* 4) Any dead cell with exactly three live neighbours becomes a live cell.
-	*/
-
-	let neighbours = [
-		[-1, -1],
-		[ 0, -1],
-		[ 1, -1],
-		[-1,  0],
-		[ 1,  0],
-		[-1,  1],
-		[ 0,  1],
-		[ 1,  1]
-	];
-
-	for int::range(0, grid.len() as int) |row| {
-		for int::range(0, grid[row].len() as int) |col| {
-			let mut count = 0;
-
-			for int::range(0, neighbours.len() as int) |n| {
-				let x = neighbours[n][0] + row;
-				let y = neighbours[n][1] + col;
-
-				if in_bounds(x, y, grid.len() as int, grid[row].len() as int) {
-					count +=  match grid[x][y] {
-						true => 1,
-						false => 0
-					};
-				}
-
-				// stop counting, the cell is dead
-				if count >= 4 { break; }
-			}
-			next[row][col] = alive(grid[row][col], count);
-		}
-	}
-}
-
 
 fn main() {
 
@@ -160,19 +41,17 @@ fn main() {
 
 	let input: &str = opt_str(&opt_matches, "i");
 	let input_path = ~path::Path(input);
-	let mut grid: ~[~[bool]] = load(input_path);
-	let mut next: ~[~[bool]] = grid.clone();
+	let mut g: ~grid::Grid = grid::Grid::load(input_path);
 
 	util::clear();
-	print_grid(grid);
+	g.print();
 	util::sleep(500);
 
 	loop {
 		util::clear();
 		//let user = io::stdin();
-		game_of_life_step(grid, next);
-		print_grid(next);
-		copy_grid(next, grid);
+		g.step();
+		g.print();
 		//user.read_line();
 		util::sleep(500);
 	}
